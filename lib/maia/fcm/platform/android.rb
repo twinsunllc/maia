adf21a739f2de853bbd32a8813047e24ee3e8b0d
Wilfred Bayudan <wilfred.bayudan@twinsunsolutions.com>
2023-09-30 07:17:59 +0800

    Try passing in android hash

--- lib/maia/fcm/platform/android.rb
+++ lib/maia/fcm/platform/android.rb
@@ -31,18 +31,25 @@ module Maia
         end
 
         def to_h
+          notification_hash = {
+            color: color,
+            sound: sound
+          }.compact
+        
+          android_hash = {}
+          android_hash[:android_channel_id] = android_channel_id if android_channel_id
+        
           hash = {
             priority: priority.to_s,
-            notification: {
-              color: color,
-              sound: sound,
-              android_channel_id: android_channel_id,
-            }.compact
+            notification: notification_hash
           }
+          
+          hash[:android] = android_hash unless android_hash.empty?
           hash[:collapse_key] = collapse_key if collapse_key
           hash[:ttl] = @message.time_to_live if @message.respond_to? :time_to_live
+          
           hash
-        end
+        end        
       end
     end
   end
